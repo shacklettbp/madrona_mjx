12c554d1f76631466cce81846e277f4704e1c993
Brennan Shacklett <bpshacklett@gmail.com>
2024-04-10 19:32:50 -0700

    Initial barkour setup

--- scripts/sim.py
+++ scripts/sim.py
@@ -1,30 +1,369 @@
 from madrona_mjx_sim import SimManager, madrona
 import math
+import sys
+
+from brax import base
+from brax import envs
+from brax import math
+from brax.base import Base, Motion, Transform
+from brax.envs.base import Env, PipelineEnv, State
+from brax.mjx.base import State as MjxState
+from brax.io import html, mjcf, model
+
+from etils import epath
+
+import mujoco
+from mujoco import mjx
+
+class BarkourEnv(PipelineEnv):
+  """Environment for training the barkour quadruped joystick policy in MJX."""
+
+  def __init__(
+      self,
+      obs_noise: float = 0.05,
+      action_scale: float = 0.3,
+      kick_vel: float = 0.05,
+      **kwargs,
+  ):
+    path = epath.Path('external/mujoco_menagerie/google_barkour_vb/scene_mjx.xml')
+    sys = mjcf.load(path.as_posix())
+    self._dt = 0.02  # this environment is 50 fps
+    sys = sys.tree_replace({'opt.timestep': 0.004, 'dt': 0.004})
+
+    # override menagerie params for smoother policy
+    sys = sys.replace(
+        dof_damping=sys.dof_damping.at[6:].set(0.5239),
+        actuator_gainprm=sys.actuator_gainprm.at[:, 0].set(35.0),
+        actuator_biasprm=sys.actuator_biasprm.at[:, 1].set(-35.0),
+    )
+
+    n_frames = kwargs.pop('n_frames', int(self._dt / sys.opt.timestep))
+    super().__init__(sys, backend='mjx', n_frames=n_frames)
+
+    self.reward_config = get_config()
+    # set custom from kwargs
+    for k, v in kwargs.items():
+      if k.endswith('_scale'):
+        self.reward_config.rewards.scales[k[:-6]] = v
+
+    self._torso_idx = mujoco.mj_name2id(
+        sys.mj_model, mujoco.mjtObj.mjOBJ_BODY.value, 'torso'
+    )
+    self._action_scale = action_scale
+    self._obs_noise = obs_noise
+    self._kick_vel = kick_vel
+    self._init_q = jp.array(sys.mj_model.keyframe('home').qpos)
+    self._default_pose = sys.mj_model.keyframe('home').qpos[7:]
+    self.lowers = jp.array([-0.7, -1.0, 0.05] * 4)
+    self.uppers = jp.array([0.52, 2.1, 2.1] * 4)
+    feet_site = [
+        'foot_front_left',
+        'foot_hind_left',
+        'foot_front_right',
+        'foot_hind_right',
+    ]
+    feet_site_id = [
+        mujoco.mj_name2id(sys.mj_model, mujoco.mjtObj.mjOBJ_SITE.value, f)
+        for f in feet_site
+    ]
+    assert not any(id_ == -1 for id_ in feet_site_id), 'Site not found.'
+    self._feet_site_id = np.array(feet_site_id)
+    lower_leg_body = [
+        'lower_leg_front_left',
+        'lower_leg_hind_left',
+        'lower_leg_front_right',
+        'lower_leg_hind_right',
+    ]
+    lower_leg_body_id = [
+        mujoco.mj_name2id(sys.mj_model, mujoco.mjtObj.mjOBJ_BODY.value, l)
+        for l in lower_leg_body
+    ]
+    assert not any(id_ == -1 for id_ in lower_leg_body_id), 'Body not found.'
+    self._lower_leg_body_id = np.array(lower_leg_body_id)
+    self._foot_radius = 0.0175
+    self._nv = sys.nv
+
+  def sample_command(self, rng: jax.Array) -> jax.Array:
+    lin_vel_x = [-0.6, 1.5]  # min max [m/s]
+    lin_vel_y = [-0.8, 0.8]  # min max [m/s]
+    ang_vel_yaw = [-0.7, 0.7]  # min max [rad/s]
+
+    _, key1, key2, key3 = jax.random.split(rng, 4)
+    lin_vel_x = jax.random.uniform(
+        key1, (1,), minval=lin_vel_x[0], maxval=lin_vel_x[1]
+    )
+    lin_vel_y = jax.random.uniform(
+        key2, (1,), minval=lin_vel_y[0], maxval=lin_vel_y[1]
+    )
+    ang_vel_yaw = jax.random.uniform(
+        key3, (1,), minval=ang_vel_yaw[0], maxval=ang_vel_yaw[1]
+    )
+    new_cmd = jp.array([lin_vel_x[0], lin_vel_y[0], ang_vel_yaw[0]])
+    return new_cmd
+
+  def reset(self, rng: jax.Array) -> State:  # pytype: disable=signature-mismatch
+    rng, key = jax.random.split(rng)
+
+    pipeline_state = self.pipeline_init(self._init_q, jp.zeros(self._nv))
+
+    state_info = {
+        'rng': rng,
+        'last_act': jp.zeros(12),
+        'last_vel': jp.zeros(12),
+        'command': self.sample_command(key),
+        'last_contact': jp.zeros(4, dtype=bool),
+        'feet_air_time': jp.zeros(4),
+        'rewards': {k: 0.0 for k in self.reward_config.rewards.scales.keys()},
+        'kick': jp.array([0.0, 0.0]),
+        'step': 0,
+    }
+
+    obs_history = jp.zeros(15 * 31)  # store 15 steps of history
+    obs = self._get_obs(pipeline_state, state_info, obs_history)
+    reward, done = jp.zeros(2)
+    metrics = {'total_dist': 0.0}
+    for k in state_info['rewards']:
+      metrics[k] = state_info['rewards'][k]
+    state = State(pipeline_state, obs, reward, done, metrics, state_info)  # pytype: disable=wrong-arg-types
+    return state
+
+  def step(self, state: State, action: jax.Array) -> State:  # pytype: disable=signature-mismatch
+    rng, cmd_rng, kick_noise_2 = jax.random.split(state.info['rng'], 3)
+
+    # kick
+    push_interval = 10
+    kick_theta = jax.random.uniform(kick_noise_2, maxval=2 * jp.pi)
+    kick = jp.array([jp.cos(kick_theta), jp.sin(kick_theta)])
+    kick *= jp.mod(state.info['step'], push_interval) == 0
+    qvel = state.pipeline_state.qvel  # pytype: disable=attribute-error
+    qvel = qvel.at[:2].set(kick * self._kick_vel + qvel[:2])
+    state = state.tree_replace({'pipeline_state.qvel': qvel})
+
+    # physics step
+    motor_targets = self._default_pose + action * self._action_scale
+    motor_targets = jp.clip(motor_targets, self.lowers, self.uppers)
+    pipeline_state = self.pipeline_step(state.pipeline_state, motor_targets)
+    x, xd = pipeline_state.x, pipeline_state.xd
+
+    # observation data
+    obs = self._get_obs(pipeline_state, state.info, state.obs)
+    joint_angles = pipeline_state.q[7:]
+    joint_vel = pipeline_state.qd[6:]
+
+    # foot contact data based on z-position
+    foot_pos = pipeline_state.site_xpos[self._feet_site_id]  # pytype: disable=attribute-error
+    foot_contact_z = foot_pos[:, 2] - self._foot_radius
+    contact = foot_contact_z < 1e-3  # a mm or less off the floor
+    contact_filt_mm = contact | state.info['last_contact']
+    contact_filt_cm = (foot_contact_z < 3e-2) | state.info['last_contact']
+    first_contact = (state.info['feet_air_time'] > 0) * contact_filt_mm
+    state.info['feet_air_time'] += self.dt
+
+    # done if joint limits are reached or robot is falling
+    up = jp.array([0.0, 0.0, 1.0])
+    done = jp.dot(math.rotate(up, x.rot[self._torso_idx - 1]), up) < 0
+    done |= jp.any(joint_angles < self.lowers)
+    done |= jp.any(joint_angles > self.uppers)
+    done |= pipeline_state.x.pos[self._torso_idx - 1, 2] < 0.18
+
+    # reward
+    rewards = {
+        'tracking_lin_vel': (
+            self._reward_tracking_lin_vel(state.info['command'], x, xd)
+        ),
+        'tracking_ang_vel': (
+            self._reward_tracking_ang_vel(state.info['command'], x, xd)
+        ),
+        'lin_vel_z': self._reward_lin_vel_z(xd),
+        'ang_vel_xy': self._reward_ang_vel_xy(xd),
+        'orientation': self._reward_orientation(x),
+        'torques': self._reward_torques(pipeline_state.qfrc_actuator),  # pytype: disable=attribute-error
+        'action_rate': self._reward_action_rate(action, state.info['last_act']),
+        'stand_still': self._reward_stand_still(
+            state.info['command'], joint_angles,
+        ),
+        'feet_air_time': self._reward_feet_air_time(
+            state.info['feet_air_time'],
+            first_contact,
+            state.info['command'],
+        ),
+        'foot_slip': self._reward_foot_slip(pipeline_state, contact_filt_cm),
+        'termination': self._reward_termination(done, state.info['step']),
+    }
+    rewards = {
+        k: v * self.reward_config.rewards.scales[k] for k, v in rewards.items()
+    }
+    reward = jp.clip(sum(rewards.values()) * self.dt, 0.0, 10000.0)
+
+    # state management
+    state.info['kick'] = kick
+    state.info['last_act'] = action
+    state.info['last_vel'] = joint_vel
+    state.info['feet_air_time'] *= ~contact_filt_mm
+    state.info['last_contact'] = contact
+    state.info['rewards'] = rewards
+    state.info['step'] += 1
+    state.info['rng'] = rng
+
+    # sample new command if more than 500 timesteps achieved
+    state.info['command'] = jp.where(
+        state.info['step'] > 500,
+        self.sample_command(cmd_rng),
+        state.info['command'],
+    )
+    # reset the step counter when done
+    state.info['step'] = jp.where(
+        done | (state.info['step'] > 500), 0, state.info['step']
+    )
+
+    # log total displacement as a proxy metric
+    state.metrics['total_dist'] = math.normalize(x.pos[self._torso_idx - 1])[1]
+    state.metrics.update(state.info['rewards'])
+
+    done = jp.float32(done)
+    state = state.replace(
+        pipeline_state=pipeline_state, obs=obs, reward=reward, done=done
+    )
+    return state
+
+  def _get_obs(
+      self,
+      pipeline_state: base.State,
+      state_info: dict[str, Any],
+      obs_history: jax.Array,
+  ) -> jax.Array:
+    inv_torso_rot = math.quat_inv(pipeline_state.x.rot[0])
+    local_rpyrate = math.rotate(pipeline_state.xd.ang[0], inv_torso_rot)
+
+    obs = jp.concatenate([
+        jp.array([local_rpyrate[2]]) * 0.25,                 # yaw rate
+        math.rotate(jp.array([0, 0, -1]), inv_torso_rot),    # projected gravity
+        state_info['command'] * jp.array([2.0, 2.0, 0.25]),  # command
+        pipeline_state.q[7:] - self._default_pose,           # motor angles
+        state_info['last_act'],                              # last action
+    ])
+
+    # clip, noise
+    obs = jp.clip(obs, -100.0, 100.0) + self._obs_noise * jax.random.uniform(
+        state_info['rng'], obs.shape, minval=-1, maxval=1
+    )
+    # stack observations through time
+    obs = jp.roll(obs_history, obs.size).at[:obs.size].set(obs)
+
+    return obs
+
+  # ------------ reward functions----------------
+  def _reward_lin_vel_z(self, xd: Motion) -> jax.Array:
+    # Penalize z axis base linear velocity
+    return jp.square(xd.vel[0, 2])
+
+  def _reward_ang_vel_xy(self, xd: Motion) -> jax.Array:
+    # Penalize xy axes base angular velocity
+    return jp.sum(jp.square(xd.ang[0, :2]))
+
+  def _reward_orientation(self, x: Transform) -> jax.Array:
+    # Penalize non flat base orientation
+    up = jp.array([0.0, 0.0, 1.0])
+    rot_up = math.rotate(up, x.rot[0])
+    return jp.sum(jp.square(rot_up[:2]))
+
+  def _reward_torques(self, torques: jax.Array) -> jax.Array:
+    # Penalize torques
+    return jp.sqrt(jp.sum(jp.square(torques))) + jp.sum(jp.abs(torques))
+
+  def _reward_action_rate(
+      self, act: jax.Array, last_act: jax.Array
+  ) -> jax.Array:
+    # Penalize changes in actions
+    return jp.sum(jp.square(act - last_act))
+
+  def _reward_tracking_lin_vel(
+      self, commands: jax.Array, x: Transform, xd: Motion
+  ) -> jax.Array:
+    # Tracking of linear velocity commands (xy axes)
+    local_vel = math.rotate(xd.vel[0], math.quat_inv(x.rot[0]))
+    lin_vel_error = jp.sum(jp.square(commands[:2] - local_vel[:2]))
+    lin_vel_reward = jp.exp(
+        -lin_vel_error / self.reward_config.rewards.tracking_sigma
+    )
+    return lin_vel_reward
+
+  def _reward_tracking_ang_vel(
+      self, commands: jax.Array, x: Transform, xd: Motion
+  ) -> jax.Array:
+    # Tracking of angular velocity commands (yaw)
+    base_ang_vel = math.rotate(xd.ang[0], math.quat_inv(x.rot[0]))
+    ang_vel_error = jp.square(commands[2] - base_ang_vel[2])
+    return jp.exp(-ang_vel_error / self.reward_config.rewards.tracking_sigma)
+
+  def _reward_feet_air_time(
+      self, air_time: jax.Array, first_contact: jax.Array, commands: jax.Array
+  ) -> jax.Array:
+    # Reward air time.
+    rew_air_time = jp.sum((air_time - 0.1) * first_contact)
+    rew_air_time *= (
+        math.normalize(commands[:2])[1] > 0.05
+    )  # no reward for zero command
+    return rew_air_time
+
+  def _reward_stand_still(
+      self,
+      commands: jax.Array,
+      joint_angles: jax.Array,
+  ) -> jax.Array:
+    # Penalize motion at zero commands
+    return jp.sum(jp.abs(joint_angles - self._default_pose)) * (
+        math.normalize(commands[:2])[1] < 0.1
+    )
+
+  def _reward_foot_slip(
+      self, pipeline_state: base.State, contact_filt: jax.Array
+  ) -> jax.Array:
+    # get velocities at feet which are offset from lower legs
+    # pytype: disable=attribute-error
+    pos = pipeline_state.site_xpos[self._feet_site_id]  # feet position
+    feet_offset = pos - pipeline_state.xpos[self._lower_leg_body_id]
+    # pytype: enable=attribute-error
+    offset = base.Transform.create(pos=feet_offset)
+    foot_indices = self._lower_leg_body_id - 1  # we got rid of the world body
+    foot_vel = offset.vmap().do(pipeline_state.xd.take(foot_indices)).vel
+
+    # Penalize large feet velocity for feet that are in contact with the ground.
+    return jp.sum(jp.square(foot_vel[:, :2]) * contact_filt.reshape((-1, 1)))
+
+  def _reward_termination(self, done: jax.Array, step: jax.Array) -> jax.Array:
+    return done & (step < 500)
+
+  def render(
+      self, trajectory: List[base.State], camera: str | None = None
+  ) -> Sequence[np.ndarray]:
+    camera = camera or 'track'
+    return super().render(trajectory, camera=camera)
+
+envs.register_environment('barkour', BarkourEnv)
 
 class Simulator:
-    def __init__(self, gpu_id, num_worlds, cpu_madrona, viz_gpu_hdls=None):
-        # Initialize madrona simulator
-        
-        self.madrona = SimManager(
-            exec_mode = madrona.ExecMode.CPU if cpu_madrona else madrona.ExecMode.CUDA,
-            gpu_id = gpu_id,
-            num_worlds = num_worlds,
-            max_episode_length = 500,
-            enable_batch_renderer = True,
-            batch_render_view_width = 64,
-            batch_render_view_height = 64,
-            visualizer_gpu_handles = viz_gpu_hdls,
-        )
-        self.madrona.init()
-        
-        self.depth = self.madrona.depth_tensor().to_torch()
-        self.rgb = self.madrona.rgb_tensor().to_torch()
+  def __init__(self, gpu_id, num_worlds, cpu_madrona, viz_gpu_hdls=None):
+    # Initialize madrona simulator
+    
+    self.madrona = SimManager(
+        exec_mode = madrona.ExecMode.CPU if cpu_madrona else madrona.ExecMode.CUDA,
+        gpu_id = gpu_id,
+        num_worlds = num_worlds,
+        max_episode_length = 500,
+        enable_batch_renderer = True,
+        batch_render_view_width = 64,
+        batch_render_view_height = 64,
+        visualizer_gpu_handles = viz_gpu_hdls,
+    )
+    self.madrona.init()
+    
+    self.mjx = BarkourEnv()
 
-        self.step_idx = 0
+    self.depth = self.madrona.depth_tensor().to_torch()
+    self.rgb = self.madrona.rgb_tensor().to_torch()
 
-    def step(self):
-        self.madrona.process_actions()
+  def step(self):
+    self.madrona.process_actions()
 
-        self.madrona.post_physics()
-
-        self.step_idx += 1
+    self.madrona.post_physics()
