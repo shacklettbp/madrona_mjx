efd88eb9b4b6c6952a476c340e7c42c82b4bcc97
Mustafa <mhaid@cs.toronto.edu>
2024-11-01 13:59:18 -0400

    Add benchmark script (variant of mjx benchmark)

--- scripts/viewer.py
+++ scripts/viewer.py
@@ -50,7 +50,7 @@ os.environ['XLA_FLAGS'] = xla_flags
 if __name__ == '__main__':
   
   model = load_model(args.mjcf)
-  mjx_model = mjx.put_model(model, _full_compat=True)
+  mjx_model = mjx.put_model(model)
 
   renderer = BatchRenderer(
     mjx_model, args.gpu_id, args.num_worlds, 
